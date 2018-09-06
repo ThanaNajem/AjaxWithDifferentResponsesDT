d3114e9e4f446cec4e1ac89e273525757d7a9219
ThanaNajem <thana.najem@outlook.com>
2018-09-06 10:43:04 +0300

    finally json, ajax files

--- countries.php
+++ countries.php
@@ -1,10 +1,7 @@
 <?php
-$arr = array(
-    "Palestine" => "00972",
-    "Jordan" => "00962",
-    "Lebanon" => "02541"
-);
+// $arr = array('Palestine' => '00972', 'Jordan' => '00962', 'Lebanon' => '02541');
+$arr = array("Palestine" => "00972", "Jordan" => "00962", "Lebanon" => "02541");
 
 echo json_encode($arr);
-
+  
 ?>
--- custom.js
+++ custom.js
@@ -1,81 +1,77 @@
-$(document).ready(function () {
-	/**/
+$(document).ready(function(){
+/**/
 
 	$.ajax({
 
-		url: "countries.php",
-		method: "POST",
-		dataType: "JSON",
-		success: function (data) {
+		url:"countries.php",
+		method:"POST",
+		dataType:"JSON",
+		success:function(data){ 
 
 			var jsonData = JSON.stringify(data)
-			var obj = JSON.parse(jsonData)
-			// console.log(obj);
-			// alert("object"+obj)
-			//   alert(obj.length)
+			 var obj = JSON.parse(jsonData)
+			 // console.log(obj);
+			 // alert("object"+obj)
+			 //   alert(obj.length)
+  
 
 
-			var $el = $("#CountryName");
-			$el.empty(); // remove old options
-			$el.append("<option value=" + 0 + ">Please Select</option>");
-			$.each(obj, function (value, key) {
-				$el.append("<option value='" + key + "'>" + value + "</option>");
-			});
+var $el = $("#CountryName"); 
+    $el.empty(); // remove old options
+    $el.append("<option value="+0+">Please Select</option>");
+    $.each(obj, function(value, key) {
+        $el.append("<option value='"+ key +"'>"+value+"</option>");
+    });  
 
-
+			
 		},
-		error: function (data) {
-
-			console.log(data.responseText);
+		error:function(data){
+		 
+			 console.log(data.responseText);
 		}
 
 	});
 	// event.preventDefault();
 
-	$('#CountryName').on('change', function () {
-		var psodoCountryCode = $('#CountryName').val();
-		/**/
-		var flag = false;
-		if (psodoCountryCode != 0) {
-			$.ajax({
+$('#CountryName').on('change',function(){
+var psodoCountryCode = $('#CountryName').val();
+/**/var flag = false;
+if (psodoCountryCode!=0) {$.ajax({
 
-				url: "countries.php",
-				method: "POST",
-				dataType: "JSON",
-				success: function (data) {
+		url:"countries.php",
+		method:"POST",
+		dataType:"JSON",
+		success:function(data){ 
 
-					var jsonData = JSON.stringify(data)
-					var obj = JSON.parse(jsonData)
-					console.log(obj);
-					$.each(obj, function (value, key) {
+			var jsonData = JSON.stringify(data)
+			 var obj = JSON.parse(jsonData)
+			  console.log(obj); 
+			   $.each(obj, function(value, key) {
+			    
+			   if(key==psodoCountryCode){
+flag=true;
+			   } 
+    });  
+  
+if (flag) {
 
-						if (key == psodoCountryCode) {
-							flag = true;
-						}
-					});
-
-					if (flag) {
-
-						$('#countryCode').val(psodoCountryCode);
-					}
-
-				},
-				error: function (data) {
-
-					console.log(data.responseText);
-				}
-
-			});
-		} else {
-			alert("please choose")
-
-			$('#countryCode').val('');
+  $('#countryCode').val(psodoCountryCode);
+}
+			
+		},
+		error:function(data){
+		 
+			 console.log(data.responseText);
 		}
 
+	});}
+else{alert("please choose")}
 
-		/**/
-	});
+
+
+/**/
+});
 
 	event.preventDefault();
-	/**/
+/**/
 });
