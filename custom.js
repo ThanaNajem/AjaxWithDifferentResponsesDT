68141c96a37c44260462e6fc6734f63bcd31ddda
ThanaNajem <thana.najem@outlook.com>
2018-09-06 10:41:13 +0300

    print psodoCountryCode in input

--- custom.js
+++ custom.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 			var obj = JSON.parse(jsonData)
 			// console.log(obj);
 			// alert("object"+obj)
-			 
+			//   alert(obj.length)
 
 
 			var $el = $("#CountryName");
@@ -29,6 +29,53 @@ $(document).ready(function () {
 			console.log(data.responseText);
 		}
 
-	}); 
+	});
+	// event.preventDefault();
+
+	$('#CountryName').on('change', function () {
+		var psodoCountryCode = $('#CountryName').val();
+		/**/
+		var flag = false;
+		if (psodoCountryCode != 0) {
+			$.ajax({
+
+				url: "countries.php",
+				method: "POST",
+				dataType: "JSON",
+				success: function (data) {
+
+					var jsonData = JSON.stringify(data)
+					var obj = JSON.parse(jsonData)
+					console.log(obj);
+					$.each(obj, function (value, key) {
+
+						if (key == psodoCountryCode) {
+							flag = true;
+						}
+					});
+
+					if (flag) {
+
+						$('#countryCode').val(psodoCountryCode);
+					}
+
+				},
+				error: function (data) {
+
+					console.log(data.responseText);
+				}
+
+			});
+		} else {
+			alert("please choose")
+
+			$('#countryCode').val('');
+		}
+
+
+		/**/
+	});
+
+	event.preventDefault();
 	/**/
 });
